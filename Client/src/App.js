9cf564b4a61afe3a7c082f8d02f647e6197ae445
ks21400 <21keshav007@gmail.com>
2020-07-11 14:50:20 +0530

    initial

---
+++ Client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import Incomeexpenses from './components/Incomeexpenses';
+import Transactionlist from './components/Transactionlist';
+import Addtransaction  from './components/Addtransaction';
+import {GlobalProvider} from './context/GlobalState';
+
+
+function App() {
+  return (
+    <GlobalProvider>
+    <Header/>
+    <div className = 'container'>
+      <Balance/>
+      <Incomeexpenses/>
+      <Transactionlist/>
+      <Addtransaction/>
+    </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
